34c2f88d281adbdb8623c900a0902b0f1b1f3aee
Broc Buhler <brocbuhler@gmail.com>
2025-07-20 18:44:33 -0700

    set up complete type and product classes done lists made

--- BrassAndPoem/Program.cs
+++ BrassAndPoem/Program.cs
@@ -1,7 +1,63 @@
 ﻿
 //create a "products" variable here to include at least five Product instances. Give them appropriate ProductTypeIds.
+List<Product> products = new List<Product>
+ {
+    new Product
+    {
+        DiceName = "Lightning 20",
+        Price = 5.5,
+        ProductTypeId = 3,
+        DR = 20
+    },
+    new Product
+    {
+        DiceName = "Hammer",
+        Price = 1,
+        ProductTypeId = 1,
+        DR = 12
+    },
+    new Product
+    {
+        DiceName = "Lucky Swing",
+        Price = 23,
+        ProductTypeId = 2,
+        DR = 20
+    },
+    new Product
+    {
+        DiceName = "Fates Finder",
+        Price = 2.3,
+        ProductTypeId = 2,
+        DR = 10
+    },
+    new Product
+    {
+        DiceName = "Toothpick",
+        Price = 7,
+        ProductTypeId = 3,
+        DR = 4
+    }
+ };
 
 //create a "productTypes" variable here with a List of ProductTypes, and add "Brass" and "Poem" types to the List. 
+List<ProductType> productTypes = new List<ProductType>
+{
+    new ProductType
+    {
+        Id = 1,
+        Design = "Brass",
+    },
+    new ProductType
+    {
+        Id = 2,
+        Design = "Poem",
+    },
+    new ProductType
+    {
+        Id = 3,
+        Design = "Sparky",
+    }
+};
 
 //put your greeting here
 
